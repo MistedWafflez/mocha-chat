e7596066488258de6f3e3ff2d02918c51b2b7907
MistedWafflez <ID+username@users.noreply.github.com>
2026-07-19 18:06:20 -0500

    Added links to mistedwafflez.com

--- scripts/app.js
+++ scripts/app.js
@@ -248,6 +248,9 @@ function renderServerList(servers = []) {
         <button class="server-btn" onclick="openFriendsDashboard()" title="Home">
             <img class="server-btn-img" src="/images/newLogo256.png" alt="Home">
         </button>
+        <a class="server-btn" title="Home" href="//mistedwafflez.com">
+            <img class="server-btn-img" alt="Home" src="//mistedwafflez.com/pfp.jpg">
+        </a>
         <div class="sidebar-divider"></div>
     `;
 
